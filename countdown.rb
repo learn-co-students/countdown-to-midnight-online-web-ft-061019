fd0e8ba5a9f94221224325aeb0e964ca034ffe17
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-12 20:15:46 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
 def countdown
+  countdown = 10
+  while countdown >= 0
+  puts "#{ countdown } second(s)!".upcase
+  countdown -= 1 
 end
+end
+
+countdown
+puts "happy new year!".upcase
